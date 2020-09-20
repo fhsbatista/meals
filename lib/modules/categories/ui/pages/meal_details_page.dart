d1a75db62e7ed28e94f2f0bffcd2c6acc692e9a5
Fernando Batista <fernandohsbatista@gmail.com>
2020-09-20 17:10:40 -0300

    Implemented meal details page layout

--- lib/modules/categories/ui/pages/meal_details_page.dart
+++ lib/modules/categories/ui/pages/meal_details_page.dart
@@ -15,9 +15,84 @@ class _MealDetailsPageState extends State<MealDetailsPage> {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(
-        child: Text(meal.title),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            _SectionTitle(title: 'Ingredientes'),
+            _SectionContent(
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                itemCount: meal.ingredients.length,
+                itemBuilder: (context, index) => Card(
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(meal.ingredients[index]),
+                    )),
+              ),
+            ),
+            _SectionTitle(title: 'Passos'),
+            _SectionContent(
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                itemCount: meal.steps.length,
+                itemBuilder: (context, index) => ListTile(
+                  leading: CircleAvatar(
+                    child: Text('${index + 1}'),
+                  ),
+                  title: Text(meal.steps[index]),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _SectionTitle extends StatelessWidget {
+  final String title;
+
+  _SectionTitle({@required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: Theme.of(context).textTheme.headline6,
+    );
+  }
+}
+
+class _SectionContent extends StatelessWidget {
+  final Widget child;
+
+  _SectionContent({@required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      margin: EdgeInsets.all(10),
+      height: 300,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: child,
     );
   }
 }
